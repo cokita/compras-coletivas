574cb3925dc54c2f70acdb0636a7bd1ff731b6a0
Ana Flávia Carvalho <cokitabr@gmail.com>
2018-07-24 09:07:18 -0300

    retirando bootstrap e colocando o angular-material

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule ,ReactiveFormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MaterialSharedModule } from './material-shared/material-shared.module';
 
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { AppRoutingModule } from './/app-routing.module';
 
+library.add(faCoffee, fas, far);
 
 @NgModule({
     declarations: [
@@ -17,7 +25,10 @@ import { AppRoutingModule } from './/app-routing.module';
         BrowserModule,
         ReactiveFormsModule,
         FormsModule,
-        AppRoutingModule
+        AppRoutingModule,
+        FontAwesomeModule,
+        BrowserAnimationsModule,
+        MaterialSharedModule
     ],
     providers: [],
     bootstrap: [AppComponent]
--- front/src/app/material-shared-module/material-shared-module.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import {MatButtonModule, MatCheckboxModule, MatCard, MatFormField} from '@angular/material';
-
-@NgModule({
-    imports: [MatButtonModule, MatCheckboxModule, MatCard, MatFormField],
-    exports: [MatButtonModule, MatCheckboxModule, MatCard, MatFormField],
-})
-export class MaterialSharedModuleModule { }
