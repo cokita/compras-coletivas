d9084ecaf98b885bed56467e4c7b7d1a6e6d437b
Ana Flávia Carvalho <cokitabr@gmail.com>
2018-07-27 09:30:01 -0300

    fazendo login, cadastro ainda com problema

---
+++ api/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,20 @@
+.<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder {
+
+    public function run()
+    {
+        dd(bcrypt('abcd1234'));
+        DB::table('users')->delete();
+
+        User::create(['email' => 'cokitabr@gmail.com', 
+                      'name' => 'Ana Flávia Carvalho', 
+                      'password' => bcrypt('abcd1234'), 
+                      'cellphone' => '61996170178', 
+                      'cpf' => '00331948150']);
+    }
+
+}
