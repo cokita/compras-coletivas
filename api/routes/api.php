b134124e3b6ba128031197812d95b56e12b71224
Ana Flávia Carvalho <cokitabr@gmail.com>
2018-07-27 09:27:17 -0300

    fazendo login, cadastro ainda com problema

--- api/routes/api.php
+++ api/routes/api.php
@@ -13,6 +13,14 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+Route::post('signup', 'AuthController@register');
+Route::post('login', 'AuthController@login');
+
+Route::group(['prefix' => 'auth', 'middleware' => 'jwt.auth'], function () {
+    Route::get('user', 'AuthController@user');
+    Route::post('logout', 'AuthController@logout');
 });
+Route::middleware('jwt.refresh')->get('/token/refresh', 'AuthController@refresh');
