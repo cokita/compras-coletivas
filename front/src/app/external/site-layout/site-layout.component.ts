98a9d67cc529ceb22c11476d98237c3f73c1b8a3
Ana Flávia Carvalho <cokitabr@gmail.com>
2019-07-19 18:44:30 -0300

    Atualizando front-page

--- front/src/app/external/site-layout/site-layout.component.ts
+++ front/src/app/external/site-layout/site-layout.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+
+export interface Tile {
+    color: string;
+    cols: number;
+    rows: number;
+    text: string;
+}
 
 @Component({
   selector: 'app-site-layout',
   templateUrl: './site-layout.component.html',
+  encapsulation: ViewEncapsulation.None,
   styleUrls: ['./site-layout.component.scss']
 })
 export class SiteLayoutComponent implements OnInit {
-
+    tiles: Tile[] = [
+        {text: 'One', cols: 1, rows: 2, color: 'lightblue'},
+        {text: 'Two', cols: 2, rows: 1, color: 'lightgreen'},
+        {text: 'Three', cols: 2, rows: 1, color: 'lightpink'},
+    ];
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
